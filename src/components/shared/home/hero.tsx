01fbe779b1b5920c689405f215f6716b19a0152b
iobami <ayobamialadenoye@yahoo.com>
2025-06-09 00:37:07 +0100

    portfolio updates

--- src/components/shared/home/hero.tsx
+++ src/components/shared/home/hero.tsx
@@ -1,25 +1,32 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { ArrowIcon } from "@/components/shared/icons";
+import { ArrowIcon, MoreVerticalIcon } from "@/components/shared/icons";
 
 export function Hero() {
   return (
-    <section className="app_main__hero bg-[url('/media/landing/hero-2x.png')]">
+    <section className="app_main__hero bg-[url('/media/landing/hero-2x.jpg')]">
+      <nav className="navbar-component hidden md:flex">
+        <section className="nav_brand">
+          <Image src="/media/landing/logo.png" alt="" width={100} height={100} />
+        </section>
+        <section className="nav_links_wrapper">
+          <ul className="nav_links">
+            <li>Service</li>
+            <li>Portfolio</li>
+            <li>Pricing</li>
+            <li>Blog</li>
+            <li>About</li>
+          </ul>
+        </section>
+      </nav>
+
+      <nav className="app_nav_mobile flex app_landing__mx md:hidden">
+        <Image src="/media/landing/logo-mb.png" alt="" width={100} height={100} />
+
+        <MoreVerticalIcon />
+      </nav>
+
       <div className="app_main__hero__text_area">
-        <nav className="navbar-component">
-          <section className="nav_brand">
-            <Image src="/media/landing/logo.png" alt="" width={100} height={100} />
-          </section>
-          <section className="nav_links_wrapper">
-            <ul className="nav_links">
-              <li>Service</li>
-              <li>Portfolio</li>
-              <li>Pricing</li>
-              <li>Blog</li>
-              <li>About</li>
-            </ul>
-          </section>
-        </nav>
         <div
           className="flex flex-col gap-[16px] main_text-wrapper items-center"
           style={{ fontWeight: "500" }}
--- src/components/shared/home/previous-work.tsx
+++ src/components/shared/home/previous-work.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 export function PreviousWork() {
   return (
     <section className="app_main__prev_work">
-      <div className="app_main__prev_work__text_area">
+      <div className="app_main__prev_work__text_area app_landing__mx">
         <h2>Previous Work</h2>
 
         <p>
@@ -13,55 +15,18 @@ export function PreviousWork() {
         </p>
       </div>
 
-      <div className="app_main__prev_work__portfolio app_landing__mx">
-        <div className="app_main__prev_work__portfolio__item app_main__prev_work__portfolio__item--3">
-          <div className="app_main__prev_work__portfolio__item__img">
-            <Image
-              src="/media/landing/portfolio/Design8.png"
-              alt="item 3"
-              fill
-            />
-          </div>
-        </div>
-
-        <div className="app_main__prev_work__portfolio__item app_main__prev_work__portfolio__item--2">
-          <div className="app_main__prev_work__portfolio__item__img">
-            <Image
-              src="/media/landing/portfolio/Design9.png"
-              alt="item 2"
-              fill
-            />
-          </div>
-        </div>
-
-        <div className="app_main__prev_work__portfolio__item app_main__prev_work__portfolio__item--1">
-          <div className="app_main__prev_work__portfolio__item__img">
-            <Image
-              src="/media/landing/portfolio/d-10.png"
-              alt="item 1"
-              fill
-            />
-          </div>
-        </div>
-
-        <div className="app_main__prev_work__portfolio__item app_main__prev_work__portfolio__item--2">
-          <div className="app_main__prev_work__portfolio__item__img">
-            <Image
-              src="/media/landing/portfolio/Design7.png"
-              alt="item 2.1"
-              fill
-            />
-          </div>
-        </div>
-
-        <div className="app_main__prev_work__portfolio__item app_main__prev_work__portfolio__item--3">
-          <div className="app_main__prev_work__portfolio__item__img">
-            <Image
-              src="/media/landing/portfolio/Design6.png"
-              alt="item 3.1"
-              fill
-            />
-          </div>
+      <div className="relative overflow-hidden w-full py-2">
+        <div className="whitespace-nowrap animate-scroll flex gap-4">
+          {/* <!-- Repeat your items here --> */}
+          <Image src="/media/landing/portfolio/work1.png" width={400} height={400} alt="Client 1" />
+          <Image src="/media/landing/portfolio/work2.png" width={400} height={400} alt="Client 2" />
+          <Image src="/media/landing/portfolio/work1.png" width={400} height={400} alt="Client 1" />
+          <Image src="/media/landing/portfolio/work2.png" width={400} height={400} alt="Client 2" />
+          {/* <!-- Duplicate the logos to make loop seamless --> */}
+          <Image src="/media/landing/portfolio/work1.png" width={400} height={400} alt="Client 1" />
+          <Image src="/media/landing/portfolio/work2.png" width={400} height={400} alt="Client 2" />
+          <Image src="/media/landing/portfolio/work1.png" width={400} height={400} alt="Client 1" />
+          <Image src="/media/landing/portfolio/work2.png" width={400} height={400} alt="Client 2" />
         </div>
       </div>
     </section>
